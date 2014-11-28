7410bbbe1ab5b5208308715aa7f0c863f5eeef33
KristofHermans <kristof.hermans.kh@gmail.com>
2014-11-28 16:01:09 +0100

    Video loop solution + sfeer

--- javascript/video.js
+++ javascript/video.js
@@ -1,15 +1,28 @@
 // VIDEO SCRIPT
+$time = 7; // number of seconds of your video
 
-$('.videoPlaceHolder').click(function() {
+var vid1 = document.getElementById('loop-1');
+var vid2 = document.getElementById('loop-2');
+var vid3 = document.getElementById('loop-3');
+var vid4 = document.getElementById('loop-4');
+var vid5 = document.getElementById('loop-5');
+var vid6 = document.getElementById('loop-6');
+var vid7 = document.getElementById('loop-7');
 
-  var video = '<div class="videoWrapper"><iframe id="video-player" width="640" height="360" src="' + $(this).attr('data-video') + '" frameborder="0" allowfullscreen></iframe></div>';
-  $(this).replaceWith(video);
+var vids = [vid1, vid2, vid3, vid4, vid5, vid6, vid7]
+var vids2 = []
 
-});
+function listVids() {
+	var numVids = getElements
+}
 
-$('.videoPlaceHolderMobile').click(function() {
+setInterval(function(){
+    playVid();
+},$time*1000-200);
 
-  var video = '<div class="videoWrapper"><iframe id="video-player" width="299" height="168" src="' + $(this).attr('data-video') + '" frameborder="0" allowfullscreen></iframe></div>';
-  $(this).replaceWith(video);
-
-});
+function playVid() { 
+    for (var i = 0; vids.length - 1; i++) {
+    	vids2[i].play();
+    	console.log(i);
+    };
+}
